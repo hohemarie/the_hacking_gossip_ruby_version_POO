fa52c9f77b7f8e1b1806bfe3357f13f642986424
Alice <marie.goudon@laposte.net>
2019-01-23 20:36:48 +0100

    ajout de sept lignes dans le controller.rb ajout de la class Gossip et de ses attr_reader
    les attreader sont le author et le content
    
    nous allons faire une méthode save qui sauvegardera dans un fichier CSV notre potin

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,3 +1,12 @@
 def create_gossip
   Gossip.new
 end
+
+class Gossip
+  attr_reader :author, :content
+  
+  def initialize(author, content)
+    @content = content
+    @author = author
+  end
+end
