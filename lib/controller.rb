bd34b60c9e2d46d086b7e18a8f1adcb97cfda56e
Bob <marie.bistroff@gmail.com>
2019-01-23 22:31:57 +0100

    recherche google

--- lib/controller.rb
+++ lib/controller.rb
@@ -34,11 +34,19 @@ class Gossip
     # ruby lire chaque ligne CSV 
     #fichier = File.open("fichier.csv","r") 
     File.open('fichier.csv','r')
-      CSV.foreach(f,col_sep:"\t") do |csv_row|
+      CSV.foreach(f,col_sep:"\t") do |author, content| 
+        #si je fais potin_1.author je récupère l'auteur du potin_1, 
+        #si je fais potin_2.content, je récupère le contenu du potin_2 
+        # array contenant des instances de potins du genre [potin_1, potin_2]
+        gossip_provisoire = Gossip.new(author,content)
+        all_gossips << gossip_provisoire
+        ##
         #lire la ligne du fichier csv
       end
+      return all_gossips
   end
   def destroy
+    #google fichier csv mode read write ruby
     print "quel potin voulez vous supprimer"
     potin = gets
     #retirer ce potin du csv
