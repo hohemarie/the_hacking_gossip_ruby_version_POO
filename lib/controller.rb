4c15649384632efa5e1345be7bd963a2984dcce1
Alice <marie.goudon@laposte.net>
2019-01-23 20:57:17 +0100

    modification dune ligne de code en commentaire et ajout dun second commentaire

--- lib/controller.rb
+++ lib/controller.rb
@@ -13,6 +13,7 @@ end
 
 def save 
   my_gossip = File.open("gossip.csv","a")
-  Gossip.new(author, content) >> my_gossip
+#  Gossip.new(author, content) >> my_gossip
+# le fichier necrit pas correctement les informations separees par une virgule dans le csv pour l'instant
   my_gossip.save
 end
