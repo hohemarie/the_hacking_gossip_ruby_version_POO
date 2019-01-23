cd5716c7d4376e5d28ba6206242ccdc5025d1401
Alice <marie.goudon@laposte.net>
2019-01-23 21:36:50 +0100

    modification dune ligne

--- lib/controller.rb
+++ lib/controller.rb
@@ -9,7 +9,7 @@ class Controller
     gossip.save
   end
   def index_gossips
-    #demander au model un array qui contient tous les potins que l'on a en base
+    Gossip.all #demander au model un array qui contient tous les potins que l'on a en base
     View.index_gossips.self
   end
 end
